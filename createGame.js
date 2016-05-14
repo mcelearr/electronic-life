18d5235941ca7f5a922f48c9abe148f5d9547911
mcelearr <rorymcelearney@gmail.com>
2016-05-14 22:42:47 +0100

    Dynamically create content with createGame.js

--- createGame.js
+++ createGame.js
@@ -1,37 +1,10 @@
-(function() {
-  var modalCount = 0;
-  //generate collapsible list
-  for (skill=0; skill<skills.length; skill++){
-    var li = document.createElement('li');
-    var title = document.createElement('div');
-    title.className = 'collapsible-header';
-    title.innerHTML = skills[skill][0];
-    li.appendChild(title);
-    //generate sublist
-    for (i=1; i<skills[skill].length; i++){
-      var currID = `skill${skill}=subskill${i}`;//determine id of current html element
-      var currSkill = user[currID.firstEqualSplit()[0]];
-      var skillState = currSkill[currID.firstEqualSplit()[1]];//obtain state of current subskill from user
-      //create html elements
-      var cb = createDefinedElement('div', 'className=collapsible-body');
-      var subTitle = createDefinedElement('p', `innerHTML=${skills[skill][i]}`);
-      var moreInfo = createDefinedElement('a', 'className=modal-trigger waves-effect waves-light btn', `href=#modal${modalCount}`, 'innerHTML=more info');
-      var complete = createDefinedElement('a', `className=waves-effect waves-light btn sbtn${skillState==0 ? ' red' : ''}`, 'href=#', `innerHTML=${skillState==0 ? 'Incomplete' : 'Complete'}`, `id=${currID}`);
-      var done = createDefinedElement('i', 'className=material-icons left', `innerHTML=${skillState==0 ? '' : 'done'}`, `id=${currID}check`);
-      //append html elements to list
-      complete.appendChild(done);
-      cb.appendChild(subTitle);
-      cb.appendChild(moreInfo);
-      cb.appendChild(complete);
-      li.appendChild(cb);
-      modalCount++;
-    };
-    var ul = document.getElementById('ul');
-    ul.appendChild(li);
-  }
-})();
-
-for (var i = 0; i < 5; i++) {
-  valley.turn();
-  console.log(valley.toString());
+function myFunction() {
+    var node = document.createElement("p");
+    var textnode = document.createTextNode("Hello");
+    node.appendChild(textnode);
+    document.getElementById("game").appendChild(node);
 }
+
+//console.log(valley)
+
+myFunction()
