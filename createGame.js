fcbbf1f5481090bc93466ff25a1533858320d095
Rory McElearney <rorymcelearney@gmail.com>
2016-05-17 21:39:40 +0100

    Write unformatted plan to game id

--- createGame.js
+++ createGame.js
@@ -1,10 +1,11 @@
-function myFunction() {
+for (var i = 0; i < plan.length; i++){
+  myFunction(plan[i]);
+}
+
+function myFunction(string) {
+    console.log(string);
     var node = document.createElement("p");
-    var textnode = document.createTextNode("Hello");
+    var textnode = document.createTextNode(string);
     node.appendChild(textnode);
     document.getElementById("game").appendChild(node);
 }
-
-//console.log(valley)
-
-myFunction()
