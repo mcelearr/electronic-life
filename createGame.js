6b12033127401db16c49d3eed7eee6c44dcf7664
mcelearr <rorymcelearney@gmail.com>
2016-06-18 11:17:40 +0100

    time to go in a different direction with this

--- createGame.js
+++ createGame.js
@@ -3,13 +3,10 @@ for (var i = 0; i < plan.length; i++){
 }
 
 function myFunction(string) {
-    console.log(string);
     var node = document.createElement("p");
     var textnode = document.createTextNode(valley.grid.space);
     node.appendChild(textnode);
     document.getElementById("game").appendChild(node);
 }
-
-  console.log(valley.grid.space)
 
   myFunction();
