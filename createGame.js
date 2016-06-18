cd02ddafd6e9c41fea5c51ad1188d0d78ebf11fd
mcelearr <rorymcelearney@gmail.com>
2016-06-18 11:21:24 +0100

    more readjustments

--- createGame.js
+++ createGame.js
@@ -3,10 +3,10 @@ for (var i = 0; i < plan.length; i++){
 }
 
 function myFunction(string) {
-    var node = document.createElement("p");
-    var textnode = document.createTextNode(valley.grid.space);
-    node.appendChild(textnode);
-    document.getElementById("game").appendChild(node);
+  var node = document.createElement("p");
+  var textnode = document.createTextNode(valley.grid.space);
+  node.appendChild(textnode);
+  document.getElementById("game").appendChild(node);
 }
 
 myFunction();
