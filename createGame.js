a21c0a6260c0ec75517be539ef4267d9ef2d4dd1
mcelearr <rorymcelearney@gmail.com>
2016-06-18 11:10:25 +0100

    removes jquery from html

--- createGame.js
+++ createGame.js
@@ -5,7 +5,6 @@ for (var i = 0; i < plan.length; i++){
 function myFunction(string) {
     console.log(string);
     var node = document.createElement("p");
-<<<<<<< HEAD
     var textnode = document.createTextNode(valley.grid.space);
     node.appendChild(textnode);
     document.getElementById("game").appendChild(node);
